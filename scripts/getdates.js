48003b6f9c084f682616a53e81d8eb5c19420831
martinloteroarenas <loteroarenasmartin@gmail.com>
2025-10-31 23:44:54 -0500

    wdd131 w01 home page activity

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// mi_script.js
+document.getElementById("lastModified").innerHTML = document.lastModified;
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
