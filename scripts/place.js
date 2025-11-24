2d20a2ee588bd951dfa6a22a068c158ebfa4f2a3
martinloteroarenas <loteroarenasmartin@gmail.com>
2025-11-24 18:57:13 -0500

    last changes in javascript

--- scripts/place.js
+++ scripts/place.js
@@ -3,6 +3,29 @@ const year = document.querySelector("#year");
 const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
+
+let temperature = 27
+let windSpeed = 7.4
 function calculateWindChill(temperature, windSpeed){
-    
+    if (temperature <= 10 && windSpeed > 4.8){
+
+        const vFactor = Math.pow(windSpeed, 0.16);
+        
+        const windChill = (
+            13.12 + 
+            (0.6215 * temperature) - 
+            (11.37 * vFactor) + 
+            (0.3965 * temperature * vFactor)
+        );
+        return Math.round(windChill*100)/100;
+    }
+    else{
+        return "N/A";
+    }
 }
+
+
+const windChillResult = calculateWindChill(temperature, windSpeed);
+
+const resultEindChill = document.getElementById('windChill');
+resultEindChill.textContent = windChillResult
