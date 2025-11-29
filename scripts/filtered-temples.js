c13e7910284274e11bcd77b8a971fe2e824bafdf
martinloteroarenas <loteroarenasmartin@gmail.com>
2025-11-29 18:31:50 -0500

    assingment

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,12 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', ()=>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
